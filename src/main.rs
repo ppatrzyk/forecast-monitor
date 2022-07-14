1c4994efcf2946512c7db33564e590e213bec266
Piotr Patrzyk <piotr@patrzyk.me>
2022-07-14 20:25:50 +0200

    separate func

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use clokwerk::{Scheduler, TimeUnits};
 use std::thread;
 use std::time::Duration;
 
+fn periodic_func() {
+    println!("Periodic task")
+}
+
 fn main() {
     println!("Scheduler starts...");
     let mut scheduler = Scheduler::new();
-    scheduler.every(10.seconds()).run(|| println!("Periodic task"));
-    // let thread_handle = scheduler.watch_thread(Duration::from_millis(100));
+    scheduler.every(2.seconds()).run(periodic_func);
     loop {
         scheduler.run_pending();
         thread::sleep(Duration::from_millis(100));
