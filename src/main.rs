1b60c5257efad5db2e018be0be987aea74acca7b
Piotr Patrzyk <piotr@patrzyk.me>
2022-07-14 20:11:06 +0200

    scheduler loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clokwerk::{Scheduler, TimeUnits};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Scheduler starts...");
+    let mut scheduler = Scheduler::new();
+    scheduler.every(10.seconds()).run(|| println!("Periodic task"));
+    // let thread_handle = scheduler.watch_thread(Duration::from_millis(100));
+    loop {
+        scheduler.run_pending();
+        thread::sleep(Duration::from_millis(100));
+    }
 }
