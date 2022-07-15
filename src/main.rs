39250eb0624fd0a3315bcd01c20e85125a4e7e8a
Piotr Patrzyk <piotr@patrzyk.me>
2022-07-15 11:09:18 +0200

    request happy case

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use clokwerk::{AsyncScheduler, TimeUnits};
+use std::collections::HashMap;
 use std::time::Duration;
 
-async fn periodic_func() {
+async fn periodic_func() -> () {
     println!("Periodic task")
+}
+
+// async fn req() -> Result<(), Box<dyn std::error::Error>> {
+async fn req() -> () {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await.unwrap()
+        .json::<HashMap<String, String>>()
+        .await.unwrap();
+    println!("{:#?}", resp);
+    // Ok(())
 }
 
 #[tokio::main]
@@ -10,6 +21,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Scheduler starts...");
     let mut scheduler = AsyncScheduler::new();
     scheduler.every(2.seconds()).run(periodic_func);
+    scheduler.every(5.seconds()).run(req);
     loop {
         scheduler.run_pending().await;
         tokio::time::sleep(Duration::from_millis(100)).await;
