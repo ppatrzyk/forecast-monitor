b2b6ee6b1bfceec52a0e64dd96fd5be42de711b1
Piotr Patrzyk <piotr@patrzyk.me>
2022-07-15 15:08:33 +0200

    openmeteo param passing

--- src/api_processor/mod.rs
+++ src/api_processor/mod.rs
@@ -1,5 +1,8 @@
 mod request;
-use std::future::Future;
+
+pub enum Source {
+    OpenMeteo,
+}
 
 pub async fn test_req(url: String) -> () {
     let resp = request::req(url, &[]).await;
@@ -7,20 +10,23 @@ pub async fn test_req(url: String) -> () {
 }
 
 // TODO parse_func should return some common data structure
-pub async fn process(parse_func: fn() -> String) -> () {
+pub async fn process(source: Source) -> () {
     // take processing func as arg
     // write to kafka
-    let resp = parse_func();
+
+    let resp = match source {
+        Source::OpenMeteo => open_meteo().await
+    };
+    println!("{:#?}", resp);
 }
 
-pub async fn open_meteo() -> String {
-    let url = "https://api.open-meteo.com/v1/".to_string();
+async fn open_meteo() -> String {
+    let url = "https://api.open-meteo.com/v1/forecast/?hourly=temperature_2m,rain,showers".to_string();
     let query_params = [
         ("current_weather", "true"),
         ("timezone", "UTC"),
         ("latitude", "51.11"),
         ("longitude", "17.03"),
-        ("hourly", "temperature_2m,rain,showers"),
     ];
     let resp = request::req(url, &query_params).await;
     // TODO pattern match if it's ok here, return common anyway
