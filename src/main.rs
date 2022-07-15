cfeb4951bfcfff0235f1b65f30d49c7474a49113
Piotr Patrzyk <piotr@patrzyk.me>
2022-07-15 11:36:29 +0200

    result wrapper

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clokwerk::{AsyncScheduler, TimeUnits};
+use serde_json::Value;
 use std::collections::HashMap;
 use std::time::Duration;
 
@@ -6,14 +7,17 @@ async fn periodic_func() -> () {
     println!("Periodic task")
 }
 
-// async fn req() -> Result<(), Box<dyn std::error::Error>> {
-async fn req() -> () {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await.unwrap()
-        .json::<HashMap<String, String>>()
-        .await.unwrap();
+async fn test_req() -> () {
+    let resp = req("http://ip-api.com/json/".to_string()).await;
     println!("{:#?}", resp);
-    // Ok(())
+}
+
+async fn req(url: String) -> Result<HashMap<String, Value>, Box<dyn std::error::Error>> {
+    let resp = reqwest::get(url)
+        .await?
+        .json::<HashMap<String, Value>>()
+        .await?;
+    Ok(resp)
 }
 
 #[tokio::main]
@@ -21,7 +25,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Scheduler starts...");
     let mut scheduler = AsyncScheduler::new();
     scheduler.every(2.seconds()).run(periodic_func);
-    scheduler.every(5.seconds()).run(req);
+    scheduler.every(5.seconds()).run(test_req);
     loop {
         scheduler.run_pending().await;
         tokio::time::sleep(Duration::from_millis(100)).await;
