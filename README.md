31b2fe498e81279b5aee7a2068be0a634b6333eb
Piotr Patrzyk <piotr@patrzyk.me>
2022-07-21 18:11:56 +0200

    TRASH ksql

--- README.md
+++ README.md
@@ -8,18 +8,6 @@ cp _config.yaml config.yaml
 
 and insert required values. [weatherapi.com](https://www.weatherapi.com/) and [tomorrow.io](https://www.tomorrow.io/weather-api/) require registration to obtain API keys.
 
-```bash
-# create stream
-curl -X "POST" "http://localhost:8088/ksql" \
-     -H "Accept: application/vnd.ksql.v1+json" \
-     -d @stream_create.json
-
-# query stream
-curl -X "POST" "http://localhost:8088/query" \
-     -H "Accept: application/vnd.ksql.v1+json" \
-     -d @stream_query.json
-```
-
 ```
 docker build -t forecasts .
 ```
--- site/viz.js
+++ site/viz.js
@@ -1,24 +1,6 @@
 
-const KSQL = "http://localhost:8088/query";
-
 async function viz() {
-    let query = JSON.stringify(
-        {
-            "ksql": "SELECT * FROM forecasts LIMIT 10;",
-            "streamsProperties": {
-                "ksql.streams.auto.offset.reset": "earliest"
-            }
-        }
-    )
-    opts = {
-        method: "POST",
-        headers: {
-            "Accept": "application/vnd.ksql.v1+json",
-            "Content-Type": "application/json",
-        },
-        body: query,
-    }
-    let response = await fetch(KSQL, opts);
+    let response = await fetch(url, opts);
     let data = await response.json();
     console.log(data);
 }
